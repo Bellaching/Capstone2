7752e2e3936497bc126b073a9751555326daddcc
Bellaching <119664628+Bellaching@users.noreply.github.com>
2023-12-03 20:45:27 +0800

    pinaayos ni sir 2

--- my_orders.php
+++ my_orders.php
@@ -1,24 +1,18 @@
 <?php
 // 0=pending,1 = confirmed, 2 = packed, 3 = for delivery, 4 = on the way, 5= delivered, 6=cancelled
 $pending = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 0 order by unix_timestamp(date_created) desc ");
-$confirmed = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 1  order by unix_timestamp(date_created) desc ");
-$forDelivery = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 2 order by unix_timestamp(date_created) desc ");
-$onTheWay = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 3 order by unix_timestamp(date_created) desc ");
-$delivered = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 4 order by unix_timestamp(date_created) desc ");
-$cancelled = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 5 order by unix_timestamp(date_created) desc ");
-$return = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 6 order by unix_timestamp(date_created) desc ");
+$readytoship = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 1  order by unix_timestamp(date_created) desc ");
+$delivered = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 2 order by unix_timestamp(date_created) desc ");
+$cancelled = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 3 order by unix_timestamp(date_created) desc ");
+$return = $conn->query("SELECT * FROM `order_list` where client_id = '{$_settings->userdata('id')}' and status = 4 order by unix_timestamp(date_created) desc ");
 
 
 $currentStatus = "pending"; // Default status is pending
 
 // Check the current status and update the variable accordingly
-if (isset($_GET['confirmed'])) {
-    $currentStatus = "confirmed";
-} elseif (isset($_GET['for_delivery'])) {
-    $currentStatus = "for-delivery";
-} elseif (isset($_GET['on_the_way'])) {
-    $currentStatus = "on-the-way";
-} elseif (isset($_GET['delivered'])) {
+if (isset($_GET['readytoship'])) {
+    $currentStatus = "readytoship";
+}  elseif (isset($_GET['delivered'])) {
     $currentStatus = "delivered";
 } elseif (isset($_GET['cancelled'])) {
     $currentStatus = "cancelled";
@@ -65,10 +59,9 @@ if (isset($_GET['confirmed'])) {
             <div class="navy">
                 <div class="nav flex-row " id="v-pills-tab" role="tablist" aria-orientation="horizontal">
                 <a class="nav-link <?= ($currentStatus === 'pending') ? 'active' : ''; ?>" id="v-pills-pending-tab" data-toggle="pill" href="#v-pills-pending" role="tab" aria-controls="v-pills-pending" aria-selected="<?= ($currentStatus === 'pending') ? 'true' : 'false'; ?>">Pending</a>
-                <a class="nav-link <?= ($currentStatus === 'confirmed') ? 'active' : ''; ?>" id="v-pills-confirmed-tab" data-toggle="pill" href="#v-pills-confirmed" role="tab" aria-controls="v-pills-confirmed" aria-selected="<?= ($currentStatus === 'confirmed') ? 'true' : 'false'; ?>">Confirmed</a>
+                <a class="nav-link <?= ($currentStatus === 'readytoship') ? 'active' : ''; ?>" id="v-pills-readytoship-tab" data-toggle="pill" href="#v-pills-readytoship" role="tab" aria-controls="v-pills-readytoship" aria-selected="<?= ($currentStatus === 'readytoship') ? 'true' : 'false'; ?>">Ready to ship</a>
 
-                <a class="nav-link <?= ($currentStatus === 'for-delivery') ? 'active' : ''; ?>" id="v-pills-for-delivery-tab" data-toggle="pill" href="#v-pills-for-delivery" role="tab" aria-controls="v-pills-for-delivery" aria-selected="<?= ($currentStatus === 'for-delivery') ? 'true' : 'false'; ?>">For Delivery</a>
-                <a class="nav-link <?= ($currentStatus === 'on-the-way') ? 'active' : ''; ?>" id="v-pills-on-the-way-tab" data-toggle="pill" href="#v-pills-on-the-way" role="tab" aria-controls="v-pills-on-the-way" aria-selected="<?= ($currentStatus === 'on-the-way') ? 'true' : 'false'; ?>">On the Way</a>
+              
                 <a class="nav-link <?= ($currentStatus === 'delivered') ? 'active' : ''; ?>" id="v-pills-delivered-tab" data-toggle="pill" href="#v-pills-delivered" role="tab" aria-controls="v-pills-delivered" aria-selected="<?= ($currentStatus === 'delivered') ? 'true' : 'false'; ?>">Delivered</a>
                 <a class="nav-link <?= ($currentStatus === 'cancelled') ? 'active' : ''; ?>" id="v-pills-cancelled-tab" data-toggle="pill" href="#v-pills-cancelled" role="tab" aria-controls="v-pills-cancelled" aria-selected="<?= ($currentStatus === 'cancelled') ? 'true' : 'false'; ?>">Cancelled</a>
                 <a class="nav-link <?= ($currentStatus === 'return-refund') ? 'active' : ''; ?>" id="v-pills-return-tab" data-toggle="pill" href="#v-pills-return" role="tab" aria-controls="v-pills-return" aria-selected="<?= ($currentStatus === 'return-refund') ? 'true' : 'false'; ?>">For Return/Refund</a>
@@ -129,7 +122,7 @@ if (isset($_GET['confirmed'])) {
                         </div>
                     </div>
                     <!-- Confirmed and Packed -->
-                    <div class="tab-pane fade" id="v-pills-confirmed" role="tabpanel" aria-labelledby="v-pills-confirmed-tab">
+                    <div class="tab-pane fade" id="v-pills-readytoship" role="tabpanel" aria-labelledby="v-pills-readytoship-tab">
                         <div class="card card-outline card-dark shadow rounded-0">
                             <div class="card-body">
                                 <div class="container-fluid">
@@ -155,7 +148,7 @@ if (isset($_GET['confirmed'])) {
                                         <tbody>
                                             <?php
                                             $i = 1;
-                                            while ($row = $confirmed->fetch_assoc()) :
+                                            while ($row = $readytoship->fetch_assoc()) :
                                             ?>
                                                 <tr>
                                                     <td class="text-center"><?= $i++ ?></td>
@@ -167,7 +160,7 @@ if (isset($_GET['confirmed'])) {
                                                     </td>
                                                     <td class="text-center"><?= number_format($row['total_amount'], 2) ?></td>
                                                     <td class="text-center">
-                                                        <span class="badge badge-secondary px-3 rounded-pill p-2 bg-info">Confirmed</span>
+                                                        <span class="badge badge-secondary px-3 rounded-pill p-2 bg-info">Ready to ship</span>
                                                     </td>
                                                     <td class="text-center">
                                                         <button class="btn btn-flat btn-sm btn-default border view_data" type="button" data-id="<?= $row['id'] ?>"><i class="fa fa-eye"></i> View</button>
@@ -180,110 +173,7 @@ if (isset($_GET['confirmed'])) {
                             </div>
                         </div>
                     </div>
-                    <!-- For Delivery -->
-                    <div class="tab-pane fade" id="v-pills-for-delivery" role="tabpanel" aria-labelledby="v-pills-for-delivery-tab">
-                        <div class="card card-outline card-dark shadow rounded-0">
-                            <div class="card-body">
-                                <div class="container-fluid">
-                                    <table class="table table-stripped">
-                                        <colgroup>
-                                            <col width="5%">
-                                            <col width="20%">
-                                            <col width="25%">
-                                            <col width="20%">
-                                            <col width="15%">
-                                            <col width="15%">
-                                        </colgroup>
-                                        <thead>
-                                            <tr class="bg-gradient-dark text-light">
-                                                <th class="text-center">#</th>
-                                                <th class="text-center">Date Ordered</th>
-                                                <th class="text-center">Ref. Code</th>
-                                                <th class="text-center">Total Amount</th>
-                                                <th class="text-center">Status</th>
-                                                <th class="text-center">Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            $i = 1;
-                                            while ($row = $forDelivery->fetch_assoc()) :
-                                            ?>
-                                                <tr>
-                                                    <td class="text-center"><?= $i++ ?></td>
-                                                    <td>
-                                                        <p class="m-0 text-center"><?= date("Y-m-d H:i", strtotime($row['date_created'])) ?></p>
-                                                    </td>
-                                                    <td>
-                                                        <p class="m-0 text-center"><?= $row['ref_code'] ?></p>
-                                                    </td>
-                                                    <td class="text-center"><?= number_format($row['total_amount'], 2) ?></td>
-                                                    <td class="text-center">
-                                                        <span class="badge badge-secondary px-3 rounded-pill p-2 bg-primary">For Delivery</span>
-                                                    </td>
-                                                    <td class="text-center">
-                                                        <button class="btn btn-flat btn-sm btn-default border view_data" type="button" data-id="<?= $row['id'] ?>"><i class="fa fa-eye"></i> View</button>
-                                                    </td>
-                                                </tr>
-                                            <?php endwhile; ?>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- On The Way -->
-                    <div class="tab-pane fade" id="v-pills-on-the-way" role="tabpanel" aria-labelledby="v-pills-on-the-way-tab">
-                        <div class="card card-outline card-dark shadow rounded-0">
-                            <div class="card-body">
-                                <div class="container-fluid">
-                                    <table class="table table-stripped">
-                                        <colgroup>
-                                            <col width="5%">
-                                            <col width="20%">
-                                            <col width="25%">
-                                            <col width="20%">
-                                            <col width="15%">
-                                            <col width="15%">
-                                        </colgroup>
-                                        <thead>
-                                            <tr class="bg-gradient-dark text-light">
-                                                <th class="text-center">#</th>
-                                                <th class="text-center">Date Ordered</th>
-                                                <th class="text-center">Ref. Code</th>
-                                                <th class="text-center">Total Amount</th>
-                                                <th class="text-center">Status</th>
-                                                <th class="text-center">Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            $i = 1;
-                                            while ($row = $onTheWay->fetch_assoc()) :
-                                            ?>
-                                                <tr>
-                                                    <td class="text-center"><?= $i++ ?></td>
-                                                    <td>
-                                                        <p class="m-0 text-center"><?= date("Y-m-d H:i", strtotime($row['date_created'])) ?></p>
-                                                    </td>
-                                                    <td>
-                                                        <p class="m-0 text-center"><?= $row['ref_code'] ?></p>
-                                                    </td>
-                                                    <td class="text-center"><?= number_format($row['total_amount'], 2) ?></td>
-                                                    <td class="text-center">
-                                                        <span class="badge badge-secondary px-3 rounded-pill" style="color: black;">On the Way</span>
-                                                    </td>
-                                                    <td class="text-center">
-                                                        <button class="btn btn-flat btn-sm btn-default border view_data" type="button" data-id="<?= $row['id'] ?>"><i class="fa fa-eye"></i> View</button>
-                                                    </td>
-                                                </tr>
-                                            <?php endwhile; ?>
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                  
                     <!-- Delivered -->
                     <div class="tab-pane fade" id="v-pills-delivered" role="tabpanel" aria-labelledby="v-pills-delivered">
                         <div class="card card-outline card-dark shadow rounded-0">
