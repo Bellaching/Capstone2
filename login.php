6fb22c503e786a3690c1af03870fffd7ee8d2505
Bellaching <119664628+Bellaching@users.noreply.github.com>
2023-12-10 10:19:23 +0800

    upedate

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php require_once('inc/header.php');  ?>
-
+<head>
 <!-- Montserrat Font (800 weight) -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap">
 
@@ -12,7 +12,11 @@
 <!-- Montserrat Font (800 weight) and Poppins Font (200 weight) -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Poppins:wght@200;200&display=swap">
 <link rel="stylesheet" href="./css/login.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SYF8M9+8APB7D1D2tLXrp5t5+kjBd5p8JwY8fF" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SYF8M9+8APB7D1D2tLXrp5t5+kjBd5p8JwY8fF" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+</head>
 <body>
   <script>
     start_loader();
@@ -54,8 +58,14 @@
           </div>
 
           <div class="back_to_shop">
-            <a href="<?php echo base_url ?>">Back to Shop</a>
-          </div>
+    <a href="<?php echo base_url ?>">
+        <i class="back fas fa-arrow-left" style="font-size: 24px;"></i>
+    </a>
+</div>
+
+
+
+
 
           <div id="login-btn">
             <button type="submit">Log in</button>
