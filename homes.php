5c75c92cc17b5650b48525887b68b7728d156af5
Bellaching <119664628+Bellaching@users.noreply.github.com>
2023-12-13 20:10:54 +0800

    update homes

--- homes.php
+++ homes.php
@@ -89,40 +89,60 @@
             flex-wrap: nowrap;
             align-content: center;
             justify-content: center;
+            
         }
+        
+
+        .row-best{
+           background-color: #F5F5F5;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            padding: 5%;
+            box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.1);
+            position: relative; /* Make sure the container is positioned relative */
+        }
+
+        
         .header_product_home_ctn{
-            padding:50px;
+            padding:10px;
         }
         .header_product_home_ctn a {
             background: #0d6efd;
             color: white;
-            padding: 15px 60px;
-            border-radius: 41px;
+            padding: 5px 20px;
+            
+           
         }
         .product_tn_home {
             margin-top: 45px;
         }
-        .header_product_home_ctn span {
-            color: #0d6efd;
+        .header_product_home_ctn span, h2{
+            color: Black;
+
             font-weight: 700;
-            font-size: 24px;
+            font-size: 46px;
+        }
+
+        .header_product_home_ctn  h2{
+            color: Black;
+
+            font-weight: 500;
+            font-size: 26px;
         }
         section.new_arrivals h1 {
     margin-bottom: 30px;
-    padding-bottom: 30px;
+    padding: 4%;
     text-align: center;
-    font-size: 1.5rem;
+    font-size: 36px;
     letter-spacing: 1px;
     text-transform: uppercase;
-    font-weight: 600;
-    margin-top: 40px;
-    padding: 5px 30px;
-    border: 1px solid #e0e0e0;
-    width: 20%; /* Adjust the width as needed */
-    color: #ffffff;
-    background-color: #202020;
-    margin-left: auto;
-    margin-right: auto;
+    font-weight: 800;
+   
+    color:#202020;
+    
+    
 }
 
 section.new_arrivals {
@@ -141,15 +161,15 @@ section.new_arrivals {
 }
 /*----Mark---------------*/
 .feature-heading h2{
-  font-size: 1.5rem;
+  font-size: 2rem;
   color: #1b1919;
   letter-spacing: 1px;
   text-transform: uppercase;
-  font-weight: 600;
-  margin-top: 40px;
+  font-weight: 800;
+  margin-top: 30px;
   padding: 15px 45px;
-  border: 1px solid #e0e0e0;
-  box-shadow: 5px 5px 20px rgba(0, 0, 0, 0.5);
+ 
+  
 }
 
 .feature-box {
@@ -200,11 +220,7 @@ section.new_arrivals {
     
 }
 
-.card{
-    box-shadow: 0px -7px 12px -4px rgba(0,0,0,0.51);
--webkit-box-shadow: 0px -7px 12px -4px rgba(0,0,0,0.51);
--moz-box-shadow: 0px -7px 12px -4px rgba(0,0,0,0.51);
-}
+
 .product-container1{
     width: 200px;
 }
@@ -214,24 +230,27 @@ section.new_arrivals {
 }
 
 .brand-item {
-    width: 100%;
-    max-width: 200px;
-    height: 150px; /* Set a fixed height for consistency */
+    width: 100%; /* Make the brand item take 100% of its container */
+    max-width: 200px; /* Set a maximum width if needed */
     margin: 0 10px 15px;
+}
+
+.card {
+    border: none;
 }
 
 .brand-img-holder {
     margin: 2%;
-    height: 150px; /* Make sure the image holder takes the full height */
+   
 }
 
 .brand-img-holder img {
     width: 100%;
-    height: 100%;
-    
-    border-radius: 10px;
+    height: 150px;
+    object-fit: cover; /* Maintain aspect ratio */
+    mix-blend-mode: multiply;
+   
 }
-
 
         .button_bottom_home a {
             background: #0d6efd;
@@ -403,11 +422,64 @@ section.new_arrivals {
         </div>
     </section>
 
+    <section id="products-home-fw" >
+        <div class="products_home_fw">
+            <div class="container products_home_content">
+                <div class="row-best">
+                    <div class=" roww ">
+                        <div class="header_product_home">
+                            <?php
+                            $products = $conn->query("SELECT p.*, b.name AS brand, c.category, COUNT(o.product_id) AS order_count
+                                FROM product_list p
+                                INNER JOIN brand_list b ON p.brand_id = b.id
+                                INNER JOIN categories c ON p.category_id = c.id
+                                INNER JOIN order_items o ON o.product_id = p.id
+                                WHERE p.delete_flag = 0 
+                                AND p.status = 1 
+                                GROUP BY p.id
+                                ORDER BY order_count DESC
+                                LIMIT 5;");
+                            // Counter variable to keep track of displayed products
+                            $counter = 0;
+                            while ($row = $products->fetch_assoc()) :
+                                // Increment the counter
+                                $counter++;
+                                // Display your product information here
+                            ?>
+                            <div class="image_container_best_seller">
+                                <img src="<?= validate_image($row['image_path']) ?>" alt="Product Image" class="img-top"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-sm-12 header_product_home">
+                        <div class="header_product_home_ctn">
+                            <span><bold>Best Seller</bold></span>
+                            <h2><?= $row['name'] ?></h2>
+                            <p> <?= isset($description) ? html_entity_decode($description) : '' ?></p>
+                            <div class="product_tn_home">
+                                <a href="./?p=products/view_product&id=<?= $row['id'] ?>">View Product</a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                
+                <?php
+                    // Check if we have displayed 4 products, and break out of the loop if true
+                    if ($counter >= 4) {
+                        break;
+                        }
+                    endwhile;
+                ?>
+            </div>
+        </div>
+    </section>
+
     
     <section>
         <div class="container_brand">
         <div class="feature-heading">
-       <h2>Featured Brands</h2>
+       <h2><strong>Featured Brands</strong></h2>
      </div>
 
      <div class="row responsive" id="brand_list" >
@@ -425,6 +497,8 @@ section.new_arrivals {
     </div>
     <?php endwhile; ?>
 </div>
+
+
 
 <section>
 
@@ -475,126 +549,9 @@ section.new_arrivals {
     </section>
 
     
-    <section id="products-home-fw" >
-        <div class="products_home_fw">
-            <div class="container products_home_content">
-                <div class="row">
-                    <div class="col-md-4 col-sm-12">
-                        <div class="header_product_home">
-                            <?php
-                            $products = $conn->query("SELECT p.*, b.name AS brand, c.category, COUNT(o.product_id) AS order_count
-                                FROM product_list p
-                                INNER JOIN brand_list b ON p.brand_id = b.id
-                                INNER JOIN categories c ON p.category_id = c.id
-                                INNER JOIN order_items o ON o.product_id = p.id
-                                WHERE p.delete_flag = 0 
-                                AND p.status = 1 
-                                GROUP BY p.id
-                                ORDER BY order_count DESC
-                                LIMIT 1;");
-                            // Counter variable to keep track of displayed products
-                            $counter = 0;
-                            while ($row = $products->fetch_assoc()) :
-                                // Increment the counter
-                                $counter++;
-                                // Display your product information here
-                            ?>
-                            <div class="image_container_best_seller">
-                                <img src="<?= validate_image($row['image_path']) ?>" alt="Product Image" class="img-top"/>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-8 col-sm-12 header_product_home">
-                        <div class="header_product_home_ctn">
-                            <span>Best Seller</span>
-                            <h2><?= $row['name'] ?></h2>
-                            <p> <?= isset($description) ? html_entity_decode($description) : '' ?></p>
-                            <div class="product_tn_home">
-                                <a href="./?p=products/view_product&id=<?= $row['id'] ?>">View Product</a>
-                            </div>
-                            
-                        </div>
-                    </div>
-                </div>
-                
-                <?php
-                    // Check if we have displayed 4 products, and break out of the loop if true
-                    if ($counter >= 4) {
-                        break;
-                        }
-                    endwhile;
-                ?>
-            </div>
-        </div>
-    </section>
+   
 
-    <section>
-    <div class="product-review responsive-review">
-            <?php
-            $productReviews = $conn->query(
-                "SELECT pr.author_rate, pr.author_comment FROM `product_reviews` pr
-                    inner join `product_variations` pv on pr.variation_id = pv.id
-                where pr.product_id =  $id order by unix_timestamp(pr.date_created) desc;
-                "
-            );
-            while ($review = $productReviews->fetch_assoc()) :
-            ?>
-                <div class="review-section mb-3 border rounded p-3">
-                    <figure class="mb-1">
-                        <blockquote class="blockquote">
-                            <p><?= ucfirst($review['author_name']) ?></p>
-                        </blockquote>
-                        <figcaption class="blockquote-footer mb-1">
-                            <?= date("Y-m-d h:i:s A", strtotime($review['date_created'])) ?> | Variation: <?= $review['variation_name'] ?>
-                        </figcaption>
-                    </figure>
-                    <div class="review-details">
-                        <?php switch (strval($review['author_rate'])):
-                            case "1": ?>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                            <?php break;
-                            case "2": ?>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                            <?php break;
-                            case "3": ?>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                            <?php break;
-                            case "4": ?>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star-o"></i>
-                            <?php break;
-                            case "5": ?>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                                <i class="fa fa-star checked"></i>
-                            <?php break;
-                            default: ?>
-                        <?php endswitch; ?>
-                        <p class="reviewer-comments mt-3"><?= ucfirst($review['author_comment']) ?></p>
-                    </div>
-                </div>
-            <?php endwhile; ?>
-        </div>
-</div>
-
-<section>
+   
 
     
 </body>
