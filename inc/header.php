3eded56c31d35ce45aa561de77e9e61611f8a493
Bellaching <119664628+Bellaching@users.noreply.github.com>
2023-12-14 08:42:06 +0800

    update

--- inc/header.php
+++ inc/header.php
@@ -6,32 +6,45 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?= $_settings->info('systitle') ?></title>
-  <link rel="icon" href="<?= validate_image($_settings->info('logo')) ?>" />
+  <title><?php echo $_settings->info('systitle') ?></title>
+  <link rel="icon" href="<?php echo validate_image($_settings->info('logo')) ?>" />
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&display=swap" rel="stylesheet">
+  <!-- Google Font: Source Sans Pro -->
+  <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback"> -->
+  <!-- Font Awesome -->
 
-  <!-- Stylesheets -->
-  <link rel="stylesheet" href="<?= base_url ?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="<?= base_url ?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <!-- Add other stylesheets as needed -->
+  <!-- Ionicons -->
+  <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
+  <!-- Tempusdominus Bootstrap 4 -->
 
-  <!-- External CSS Files -->
-  <link rel="stylesheet" href="<?= base_url ?>dist/css/adminlte.css">
-  <link rel="stylesheet" href="<?= base_url ?>assets/css/styles.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-  <!-- External Scripts -->
-  <script src="<?= base_url ?>plugins/jquery/jquery.min.js"></script>
-  <script src="<?= base_url ?>plugins/jquery-ui/jquery-ui.min.js"></script>
-  <script src="<?= base_url ?>plugins/sweetalert2/sweetalert2.min.js"></script>
-  <script src="<?= base_url ?>plugins/toastr/toastr.min.js"></script>
-  <script src="<?= base_url ?>dist/js/script.js"></script>
-  <script src="<?= base_url ?>assets/js/scripts.js"></script>
-
-  <script>
-    var _base_url_ = '<?= base_url ?>';
-  </script>
+  <!-- Select2 -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <!--<link rel="stylesheet" href="<?php echo base_url ?>dist/css/adminlte.css">-->
+  <!--<link rel="stylesheet" href="<?php echo base_url ?>dist/css/custom.css">-->
+  <!--<link rel="stylesheet" href="<?php echo base_url ?>assets/css/styles.css">-->
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/summernote/summernote-bs4.min.css">
+  <!-- SweetAlert2 -->
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url ?>plugins/timepicker/timepicker.css">
   <style type="text/css">
     /* Firefox */
     * {
@@ -171,3 +184,4 @@
 }
   </style>
 </head>
+
