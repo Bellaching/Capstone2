5fe0fb8d02584d375a3009ce6094420fb9269371
Bellaching <119664628+Bellaching@users.noreply.github.com>
2023-12-15 22:23:45 +0800

    update

--- admin/orders/update_status.php
+++ admin/orders/update_status.php
@@ -24,15 +24,10 @@ if(isset($_GET['id'])){
         <div class="form-group">
             <label for="status" class="control-label">Status</label>
             <select name="status" id="status" class="custom-select form-control-sm">
-            
-    <option value="0" <?= isset($status) && $status == 0 ? 'selected' : '' ?>>Pending</option>
-    <option value="1" <?= isset($status) && $status == 1 ? 'selected' : '' ?>>Cancelled</option>
-    <option value="2" <?= isset($status) && $status == 2 ? 'selected' : '' ?>>Confirmed</option>
-   
-    <option value="3" <?= isset($status) && $status == 3 ? 'selected' : '' ?>>Delivered</option>
-    
-
-
+                <option value="0" <?= isset($status) && $status == 0 ? 'selected' : '' ?>>Pending</option>
+                <option value="1" <?= isset($status) && $status == 1 ? 'selected' : '' ?>>Cancelled</option>
+                <option value="2" <?= isset($status) && $status == 2 ? 'selected' : '' ?>>Confirmed</option>
+                <option value="3" <?= isset($status) && $status == 3 ? 'selected' : '' ?>>Delivered</option>
             </select>
         </div>
     </form>
@@ -41,43 +36,58 @@ if(isset($_GET['id'])){
 <script>
     $(function(){
         $('#update_order').submit(function(e){
-			e.preventDefault();
+            e.preventDefault();
             var _this = $(this)
-			 $('.err-msg').remove();
-			start_loader();
-			$.ajax({
-				url:_base_url_+"classes/Master.php?f=update_order_status",
-				data: new FormData($(this)[0]),
+            $('.err-msg').remove();
+            start_loader();
+            $.ajax({
+                url:_base_url_+"classes/Master.php?f=update_order_status",
+                data: new FormData($(this)[0]),
                 cache: false,
                 contentType: false,
                 processData: false,
                 method: 'POST',
                 type: 'POST',
                 dataType: 'json',
-				error:err=>{
-					console.log(err)
-					alert_toast("An error occured",'error');
-					end_loader();
-				},
-				success:function(resp){
-					if(typeof resp =='object' && resp.status == 'success'){
-						location.reload();
-					}else if(resp.status == 'failed' && !!resp.msg){
+                error:err=>{
+                    console.log(err)
+                    alert_toast("An error occurred",'error');
+                    end_loader();
+                },
+                success:function(resp){
+                    if(typeof resp =='object' && resp.status == 'success'){
+                        location.reload();
+                    }else if(resp.status == 'failed' && !!resp.msg){
                         var el = $('<div>')
-                            el.addClass("alert alert-danger err-msg").text(resp.msg)
-                            _this.prepend(el)
-                            el.show('slow')
-                            $("html, body").animate({ scrollTop: _this.closest('.card').offset().top }, "fast");
-                            end_loader()
+                        el.addClass("alert alert-danger err-msg").text(resp.msg)
+                        _this.prepend(el)
+                        el.show('slow')
+                        $("html, body").animate({ scrollTop: _this.closest('.card').offset().top }, "fast");
+                        end_loader()
                     }else{
-						alert_toast("An error occured",'error');
-						end_loader();
+                        alert_toast("An error occurred",'error');
+                        end_loader();
                         console.log(resp)
-					}
-				}
-			})
-		})
+                    }
+                }
+            })
+        })
+
+        // Disable options based on selected status
+        $('#status').change(function() {
+            var selectedStatus = $(this).val();
+            $('#status option').prop('disabled', false);
+            
+            if (selectedStatus == 1) { // If Cancelled is selected
+                $('#status option[value="2"]').prop('disabled', true); // Disable Confirmed
+                $('#status option[value="3"]').prop('disabled', true); // Disable Delivered
+            } else if (selectedStatus == 2) { // If Confirmed is selected
+                $('#status option[value="0"]').prop('disabled', true); // Disable Pending
+                $('#status option[value="1"]').prop('disabled', true); // Disable Cancelled
+                $('#status option[value="3"]').prop('disabled', true); // Disable Delivered
+            } else if (selectedStatus == 3) { // If Delivered is selected
+                $('#status option').prop('disabled', true); // Disable all options
+            }
+        });
     })
 </script>
-
-
