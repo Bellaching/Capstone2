43b4c254ce26276b7b6f100ccda92edf48a4d4d7
Bellaching <119664628+Bellaching@users.noreply.github.com>
2023-12-16 16:39:02 +0800

    update inquiry

--- admin/clients/messageform.php
+++ admin/clients/messageform.php
@@ -4,7 +4,18 @@ ob_start(); // Start output buffering
 include 'sendemail.php';
 
 // Retrieve data from the database
-$conn = new mysqli("localhost", "root", "", "capstone_two_db");
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+    if(!defined('base_url')) define('base_url','https://atvmotoshop.online/');
+    if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+    if(!defined('dev_data')) define('dev_data',$dev_data);
+    if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+    if(!defined('DB_USERNAME')) define('DB_USERNAME',"u738394287_arnoldtv");
+    if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"4N8cIt=&qM#c");
+    if(!defined('DB_NAME')) define('DB_NAME',"u738394287_arnoldtv");
+    
+    // Create a new mysqli connection
+    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
