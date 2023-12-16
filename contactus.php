6940edf79332e7f14d6b6c873be8717737f6e4be
Bellaching <119664628+Bellaching@users.noreply.github.com>
2023-12-16 16:31:23 +0800

    update

--- contactus.php
+++ contactus.php
@@ -14,7 +14,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     $client_id = $_settings->userdata('id'); // Assuming client_id is stored in $_settings
 
     // Connect to the database (Update the username and password if needed)
-    $conn = new mysqli("localhost", "u738394287_arnoldtv", "", "4N8cIt=&qM#c");
+    $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+    if(!defined('base_url')) define('base_url','https://atvmotoshop.online/');
+    if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+    if(!defined('dev_data')) define('dev_data',$dev_data);
+    if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+    if(!defined('DB_USERNAME')) define('DB_USERNAME',"u738394287_arnoldtv");
+    if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"4N8cIt=&qM#c");
+    if(!defined('DB_NAME')) define('DB_NAME',"u738394287_arnoldtv");
+    
+    // Create a new mysqli connection
+    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
     // Check connection
     if ($conn->connect_error) {
