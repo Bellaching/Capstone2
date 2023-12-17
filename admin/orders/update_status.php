65a6cabb432c7a825d271ec297062e8aec206591
Bellaching <119664628+Bellaching@users.noreply.github.com>
2023-12-17 08:47:12 +0800

    .

--- admin/orders/update_status.php
+++ admin/orders/update_status.php
@@ -39,45 +39,67 @@ if(isset($_GET['id'])){
 </div>
 
 <script>
-    $(function(){
-        $('#update_order').submit(function(e){
-			e.preventDefault();
-            var _this = $(this)
-			 $('.err-msg').remove();
-			start_loader();
-			$.ajax({
-				url:_base_url_+"classes/Master.php?f=update_order_status",
-				data: new FormData($(this)[0]),
-                cache: false,
-                contentType: false,
-                processData: false,
-                method: 'POST',
-                type: 'POST',
-                dataType: 'json',
-				error:err=>{
-					console.log(err)
-					alert_toast("An error occured",'error');
-					end_loader();
-				},
-				success:function(resp){
-					if(typeof resp =='object' && resp.status == 'success'){
-						location.reload();
-					}else if(resp.status == 'failed' && !!resp.msg){
-                        var el = $('<div>')
-                            el.addClass("alert alert-danger err-msg").text(resp.msg)
-                            _this.prepend(el)
-                            el.show('slow')
-                            $("html, body").animate({ scrollTop: _this.closest('.card').offset().top }, "fast");
-                            end_loader()
-                    }else{
-						alert_toast("An error occured",'error');
-						end_loader();
-                        console.log(resp)
-					}
-				}
-			})
-		})
-    })
+$(function(){
+    var statusSelect = $('#status');
+    
+    // Initial check to disable options based on the current status
+    checkStatusOptions();
+
+    $('#update_order').submit(function(e){
+        e.preventDefault();
+        var _this = $(this);
+        $('.err-msg').remove();
+        start_loader();
+        $.ajax({
+            url: _base_url_+"classes/Master.php?f=update_order_status",
+            data: new FormData($(this)[0]),
+            cache: false,
+            contentType: false,
+            processData: false,
+            method: 'POST',
+            type: 'POST',
+            dataType: 'json',
+            error: err => {
+                console.log(err);
+                alert_toast("An error occurred", 'error');
+                end_loader();
+            },
+            success: function(resp){
+                if(typeof resp == 'object' && resp.status == 'success'){
+                    location.reload();
+                } else if(resp.status == 'failed' && !!resp.msg){
+                    var el = $('<div>')
+                    el.addClass("alert alert-danger err-msg").text(resp.msg)
+                    _this.prepend(el)
+                    el.show('slow')
+                    $("html, body").animate({ scrollTop: _this.closest('.card').offset().top }, "fast");
+                    end_loader();
+                } else {
+                    alert_toast("An error occurred", 'error');
+                    end_loader();
+                    console.log(resp);
+                }
+            }
+        });
+    });
+
+    // Function to check and disable options based on the selected status
+    function checkStatusOptions() {
+        var selectedStatus = statusSelect.val();
+        if (selectedStatus == 3) { // If status is Delivered
+            statusSelect.find('option[value="0"]').prop('disabled', true); // Disable Pending
+            statusSelect.find('option[value="1"]').prop('disabled', true); // Disable Cancelled
+        } else {
+            // Enable all options if status is not Delivered
+            statusSelect.find('option').prop('disabled', false);
+        }
+    }
+
+    // Event handler for status change
+    statusSelect.on('change', function(){
+        checkStatusOptions();
+    });
+});
 </script>
 
 
